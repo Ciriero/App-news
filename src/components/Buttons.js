d4050a91ae72e592e3ea10197457741a8b2f09cd
Christian Calle C <chcallec@gmail.com>
2022-12-18 13:31:31 +0100

    added styles paginations buttons

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { useGlobalContext } from "../context/context";
 
 const Buttons = () => {
+  const { handlePage, page, tpages, isLoading } = useGlobalContext();
   return (
-    <div>Buttons</div>
-  )
-}
+    <div className="btn-container">
+      <button disabled={isLoading} onClick={() => handlePage("prev")}>
+        prev
+      </button>
+      <p>
+        {page + 1} of {tpages}
+      </p>
+      <button disabled={isLoading} onClick={() => handlePage("next")}>
+        next
+      </button>
+    </div>
+  );
+};
 
-export default Buttons
+export default Buttons;
