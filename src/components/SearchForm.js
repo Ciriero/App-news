86b69987a5d9dd0c654f9a173548eeef9ba1c942
Christian Calle C <chcallec@gmail.com>
2022-12-18 13:10:10 +0100

    added debounce with lodash.debounce

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,18 +1,30 @@
-import React from 'react';
-import { useGlobalContext } from '../context/context';
+import React, { useEffect, useMemo } from "react";
+import { useGlobalContext } from "../context/context";
+import debounce from "lodash.debounce";
 
 const SearchForm = () => {
-  const {handleSearch} = useGlobalContext()
+  const { handleSearch } = useGlobalContext();
+
+  const debounceResult = useMemo(() => {
+    return debounce(handleSearch, 1000);
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      debounceResult.cancel();
+    };
+  });
+
   return (
     <form className="search-form" onSubmit={(e) => e.preventDefault()}>
       <h2>Search News</h2>
       <input
         type="text"
         className="form-input"
-        onChange={(e) => handleSearch(e.target.value)}
+        onChange={(e) => debounceResult(e?.target?.value)}
       />
     </form>
-  )
-}
+  );
+};
 
-export default SearchForm
+export default SearchForm;
